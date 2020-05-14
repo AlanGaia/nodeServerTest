68d24ddc69089f8733c12af80d3141255c60b86d
valiantsuli <alan.j.gaia@gmail.com>
2020-05-14 20:46:35 -0300

    primer commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+//Require del modulo express
+const express = require("express");
+//require del path para hacer rutas absolutas
+const path = require("path");
+//PUERTO constante que tiene el numero del puerto
+const puerto = 3000;
+//inicializo express y lo que devuelve lo guardo en app
+const app = express();
+
+//middlewares
+app.use(express.static(path.join(__dirname, "./../client/")));
+
+//Rutas del get
+app.get("/", function (req, res) {
+  res.sendfile("index.html");
+});
+
+//Abro el servidor en el puerto = PUERTO
+app.listen(puerto, () => {
+  console.log(`Servidor encendido en el puerto ${puerto}`);
+});
